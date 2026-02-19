0352ae6ad3ce05a7d207027d2e8f43c3956675b9
intanbalqis <intanpolindra@gmail.com>
2026-02-19 12:54:45 +0700

    first commit internship test

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-const productList = document.getElementById("product-list");
+const elektronik = document.getElementById("elektronik");
+const fashion = document.getElementById("fashion");
+const aksesoris = document.getElementById("aksesoris");
 
 fetch("https://fakestoreapi.com/products")
   .then(res => res.json())
   .then(data => {
-    data.forEach(product => {
-      const col = document.createElement("div");
-      col.className = "col-md-3 mb-4";
 
-      col.innerHTML = `
-        <div class="card h-100 shadow-sm">
-          <img src="${product.image}" class="card-img-top p-3">
-          <div class="card-body">
-            <h6 class="card-title">${product.title}</h6>
-            <p class="text-success fw-bold">$${product.price}</p>
+    data.forEach(product => {
+
+      const card = `
+        <div class="col-md-3 mb-4">
+          <div class="card h-100 shadow-sm">
+            <img src="${product.image}" class="card-img-top p-3">
+            <div class="card-body">
+              <h6 class="card-title">${product.title}</h6>
+              <p class="text-success fw-bold">$${product.price}</p>
+            </div>
           </div>
         </div>
       `;
 
-      productList.appendChild(col);
+      // FILTER KATEGORI
+      if (product.category === "electronics") {
+        elektronik.innerHTML += card;
+
+      } else if (
+        product.category === "men's clothing" ||
+        product.category === "women's clothing"
+      ) {
+        fashion.innerHTML += card;
+
+      } else {
+        aksesoris.innerHTML += card;
+      }
+
     });
+
   });
